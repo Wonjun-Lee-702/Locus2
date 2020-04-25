56862ecd968d70ac2fef1e624fbffe3eb787e34f
uwang3 <uwang3@gatech.edu>
2020-04-24 23:00:50 -0400

    imagebuttons for schools

--- app/src/main/java/com/example/locus/FirstPageActivity.java
+++ app/src/main/java/com/example/locus/FirstPageActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -20,43 +22,62 @@ public class FirstPageActivity extends AppCompatActivity {
     public int school_position;
     public static Context context;
 
-    private ListView school_list;
+    private static final int[] buttonIDs = {
+            R.id.techButton,
+            R.id.emoryButton
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         final FirstPageActivity tmp = this;
         setContentView(R.layout.activity_first_page);
-        ArrayList<String> schools = new ArrayList<>();
-        schools.add("Georgia Institute of Technology");
-        schools.add("Emory University");
+//        ArrayList<ImageButton> schools = new ArrayList<>();
 
+        for (int id: buttonIDs) {
+            ImageButton button = (ImageButton) findViewById(id);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //function call on click
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    switch (v.getId()) {
+                        case R.id.techButton:
+                            school_position = 0;
+                            break;
+                        case R.id.emoryButton:
+                            school_position = 1;
+                            break;
+                    }
+                    context = tmp;
+                    startActivity(intent);
+                }
+            });
+//            schools.add(button);
+        }
 
-        school_list = findViewById(R.id.school_list);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, schools);
-        school_list.setAdapter(adapter);
-        school_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-
-                //send data to next activity
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                //intent.putExtra("school_position", position);
-                school_position = position;
-                context = tmp;
-                startActivity(intent);
-            }
-        });
-        /*
-        school_list.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //function call on click
-                Intent intent = new Intent(FirstPageActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
-        */
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, schools);
+//        school_list.setAdapter(adapter);
+//        school_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//
+//
+//                //send data to next activity
+//                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//                //intent.putExtra("school_position", position);
+//                school_position = position;
+//                context = tmp;
+//                startActivity(intent);
+//            }
+//        });
+//        school_list.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //function call on click
+//                Intent intent = new Intent(FirstPageActivity.this, MainActivity.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 }
